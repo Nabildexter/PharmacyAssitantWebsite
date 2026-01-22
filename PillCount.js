5e1b92eb6d89b7126e76e17772525c1527fa6ea9
Nabildexter <36948041+Nabildexter@users.noreply.github.com>
2026-01-21 20:16:13 -0800

    Fix stylings, some new dins, fix some sig bugs

--- PillCount.js
+++ PillCount.js
@@ -2,3 +2,11 @@ console.log("PillCount JS Succesfully Connected!");
 console.log("-------");
 console.log("");
 
+
+//Pill Counting Model
+//Jan 20th 2026 on 10 Images
+// https://rapid.roboflow.com/p/oyju2vdj
+
+
+
+
